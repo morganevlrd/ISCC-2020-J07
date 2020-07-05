324d83906fc020b555160fee5d84623895e1fb57
morganevlrd <67578362+morganevlrd@users.noreply.github.com>
2020-07-05 18:53:26 +0200

    Add files via upload

---
+++ double-tableaux.php
@@ -0,0 +1,16 @@
+<?php
+$panier = array (
+    array ("T-shirt rouge", 15,50,5),
+    array ("T-shirt vert", 15.50, 6),
+    array ("T-Shirt argent", 15.50, 8),
+    array ("Short bleu", 16.50, 5),
+    array ("Short vert",19.99,5)
+    array ("Veste argent", 19.99, 10)
+    array ("Veste marron", 35,3),
+);
+
+function affichage_panier ($panier){
+    foreach ($panier as $element) {
+
+    }
+}
